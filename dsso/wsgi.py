de37899efb083729343169149fd4917274f9d149
grischan <grischan@gmail.com>
2020-05-23 18:25:34 +0200

    sso_html

--- dsso/wsgi.py
+++ dsso/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/var/www/dsso')
 
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsso.settings')
 
