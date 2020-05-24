a06566b5a2cb9663f3e3b35326d0059910409bc8
grischan <grischan@gmail.com>
2020-05-25 01:09:25 +0200

    dastatic

--- discourseAdmin/admin.py
+++ discourseAdmin/admin.py
@@ -1,7 +1,9 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from discourseAdmin.models import Participant,dGroup, User_Groups
+
 
 # Define an inline admin descriptor for Employee model
 # which acts a bit like a singleton
@@ -15,12 +17,25 @@ class UserGroupsInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'User_Groups'
 
+class MyUserAdminForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username',)
+        #exclude = ('user_permission','group', 'date_joined') 
 
 # Define a new User admin
 class UserAdmin(BaseUserAdmin):
+    form = MyUserAdminForm
+    print(form)
     inlines = (ParticipantInline,UserGroupsInline)
+    
+    class Media:
+        css = {
+             'all': ('css/da_main.css',)
+        }
 
 admin.site.unregister(User)
+admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
 
 
